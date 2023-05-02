324dc80192110ca643c91f299c13e0ecdb4369a8
Acer <olga_savkina_min@mail.ru>
2023-05-02 10:24:02 +0300

    @author Savkina Olga

--- lab6/servlet/src/main/java/FirstServlet.java
+++ lab6/servlet/src/main/java/FirstServlet.java
@@ -7,11 +7,24 @@ import java.io.PrintWriter;
 
 public class FirstServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html");
-        PrintWriter out = resp.getWriter();
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException
+    {
+// Получаем параметры запроса
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+
+// Создаем экземпляр класса, который будет обрабатывать параметры запроса
+        FormProcessor formProcessor = new FormProcessor();
+
+// Обрабатываем параметры запроса и получаем результат
+        String result = formProcessor.processForm(name, email);
+
+// Отправляем ответ клиенту
+        PrintWriter out = response.getWriter();
         out.println("<html><body>");
-        out.println("<h1> First Servlet </h1>");
+        out.println("<h1>Form Submit Result:</h1>");
+        out.print(result);
         out.println("</body></html>");
     }
 }
--- lab6/servlet/src/main/webapp/index.jsp
+++ lab6/servlet/src/main/webapp/index.jsp
@@ -1,5 +1,17 @@
+<!DOCTYPE html>
 <html>
+<head>
+    <title>Example Form</title>
+</head>
 <body>
-<h2>Hello World!</h2>
+<form method="POST" action="hello">
+    <label for="name">Name</label>
+    <input type="text" name="Логин" id="Логин">
+    <br>
+    <label for="email"></label>
+    <input type="email" name="email" id="email">
+    <br>
+    <input type="submit" value="Submit">
+</form>
 </body>
 </html>
